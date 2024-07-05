b78de676d178595b9add521b1b33824b8df2060b
Sebastiano <sebastiano@racca.me>
2024-07-05 15:53:44 +0200

    Add demo

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,7 +1,31 @@
 <script lang="ts">
 	import NotPaid from '$lib/index.js';
+
+	let due_date: string;
+	let days_deadline: number;
+
+	let show_not_paid = false;
+
+	const createNotPaidElement = () => show_not_paid = true;
 </script>
 
-<NotPaid due_date={new Date('2024-06-05')} days_deadline={60} />
 
-<h1>Example page</h1>
+{#if show_not_paid}
+	<NotPaid due_date={new Date(due_date)} days_deadline={days_deadline} />
+{/if}
+
+<h1>Demo</h1>
+
+<form>
+	<label for="due-date">Select the Due Date:</label>
+	<input type="date" id="due-date" bind:value={due_date} required>
+
+	<br>
+
+	<label for="days-deadline">Select How many days till the website is vanished:</label>
+	<input type="number" id="days-deadline" bind:value={days_deadline} required>
+
+	<br>
+	
+	<button type="submit" on:click={createNotPaidElement}>Create NotPaid Element</button>
+</form>
